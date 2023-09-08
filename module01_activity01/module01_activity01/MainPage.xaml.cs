f53396a1fc35f55cabb87b1cbbb871ce6331fca3
Nicole Frankie Capuno <colecapuno@gmail.com>
2023-09-08 10:48:19 +0800

    first commit

---
+++ module01_activity01/module01_activity01/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace module01_activity01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OpenPage1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+        private async void OpenPage2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
